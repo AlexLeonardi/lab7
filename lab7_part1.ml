2703593dfc8ecc5e2184b8c68b8e6a25c27b9f7c
Alex Leonardi <alexleonardi@Alexs-MacBook-Pro-10.local>
2019-02-26 11:06:05 -0500

    draft 1

--- lab7_part1.ml
+++ lab7_part1.ml
@@ -36,11 +36,11 @@ functions and values that a mathematics module might contain.
 module type MATH =
   sig
     (* the constant pi *)
-    val pi : float                        
+    val pi : float
     (* cosine of an angle *)
     val cos : float -> float
     (* sine of an angle *)
-    val sin : float -> float 
+    val sin : float -> float
     (* sum of two numbers *)
     val sum : float -> float -> float
     (* maximum value in a list; None if list is empty *)
@@ -51,18 +51,22 @@ module type MATH =
 Exercise 1A: Complete the implementation of a module called Math that
 satisfies the signature above. (The value "nan" stands for "not a
 number" and is an actual value of the float type, as dictated by the
-IEEE Floating Point standard described at 
+IEEE Floating Point standard described at
 <https://en.wikipedia.org/wiki/IEEE_754>. We're using it here as a
 temporary value pending your putting in appropriate ones.)
 ......................................................................*)
 
 module Math : MATH =
   struct
-    let pi = nan
-    let cos _ = nan
-    let sin _ = nan
-    let sum _ _ = nan
-    let max _ = None
+    let pi = 3.1415926535897932384626433832795
+    let cos (x : float) : float = cos x
+    let sin (x : float) : float = sin x
+    let sum (x : float) (y : float) : float = x +. y
+    let max (lst : float list) : float option =
+      match lst with
+      | [] -> None
+      | _ -> Some
+        (List.fold_left (fun x acc -> max x acc) min_float lst)
   end ;;
 
 (*......................................................................
@@ -72,7 +76,7 @@ type float option. Name the resulting value "result". (Do not use a
 local open for this exercise.)
 ......................................................................*)
 
-let result = Some nan ;;
+let result = Math.max [Math.cos Math.pi, Math.sin Math.pi] ;;
 
 (*......................................................................
 Exercise 1C: Reimplement the computation from 1B above, now as
